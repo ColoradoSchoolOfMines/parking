4428bcb828da5285a0b217b33d160a9cf79c402e
Andrew DeMaria <ademaria@mines.edu>
2013-03-26 13:11:10 -0600

    Added map overlay capability to image.  Links to lot_path

--- app/helpers/lots_helper.rb
+++ app/helpers/lots_helper.rb
@@ -1,2 +1,12 @@
 module LotsHelper
+    def get_coords(lot)
+        # returns a comma separted list of coordinates for use in an html map.
+        # i.e. x1,y1,x2,y2,...etc
+        coordinates = lot.coordinates.collect do |coord|
+            "#{coord.x},#{coord.y}"
+        end
+
+        coordinates.join(',')
+    end
+
 end
