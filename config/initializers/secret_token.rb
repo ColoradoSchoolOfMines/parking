90e8a5f48ef33e97a453452055136a8b91881be7
Andrew DeMaria <ademaria@mines.edu>
2013-04-22 11:43:07 -0600

    Made secret token reference the envirnoment var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Parking::Application.config.secret_token = 'f77ef91d675683f9ad52f4fdbaf1fb3865368e2e35b9e6b46a9acb3ef5bb085912d52ca254e1d9421b71ed46d8a8875261aecb6fd335bb0fd8f91da63231cc0c'
+Parking::Application.config.secret_token = ENV["RAILS_PARKING_TOKEN"]'
