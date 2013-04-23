5e0323f03a9cf639deac515ecd19ff3dbd66370f
Andrew DeMaria <ademaria@mines.edu>
2013-04-22 20:34:44 -0600

    Dynamic calculation of image map based on image size

--- app/helpers/lots_helper.rb
+++ app/helpers/lots_helper.rb
@@ -1,12 +1,25 @@
 module LotsHelper
-    def get_coords(lot)
-        # returns a comma separted list of coordinates for use in an html map.
-        # i.e. x1,y1,x2,y2,...etc
-        coordinates = lot.coordinates.collect do |coord|
-            "#{coord.x},#{coord.y}"
-        end
+    def get_coords(lot, size=:medium)
+      # returns a comma separted list of coordinates for use in an html map.
+      # i.e. x1,y1,x2,y2,...etc
+      x_resize_ratio = get_ratio_for_dimension( size, Parking::Application.config.x )
+      y_resize_ratio = get_ratio_for_dimension( size, Parking::Application.config.y )
 
-        coordinates.join(',')
+      coordinates = lot.coordinates.collect do |coord|
+        "#{(coord.x * x_resize_ratio).ceil},#{(coord.y * y_resize_ratio).ceil}"
+      end
+
+      coordinates.join(',')
+    end
+
+    def get_image_name( size=:medium )
+      "parking_square_#{size.to_s}.png"
+    end
+
+    private
+
+    def get_ratio_for_dimension( size, dim )
+      Parking::Application.config.parking_image_size[size][dim] / Parking::Application.config.parking_image_size[:original][dim].to_f
     end
 
 end
---
+++ config/initializers/image_sizes.rb
@@ -0,0 +1,10 @@
+Parking::Application.config.parking_image_size = {
+  original: [1100,1100],
+  medium: [800,800],
+  small: [400,400]
+}
+
+Parking::Application.config.x = 0
+Parking::Application.config.y = 1
+
+
